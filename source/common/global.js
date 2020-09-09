7c404fbed8302b0b74290ded6946cd6a097e367f
tbiinfotech <tbiinfotech@gmail.com>
2020-09-09 18:17:13 +0530

    injoy android code

---
+++ source/common/global.js
@@ -0,0 +1,4 @@
+export const base_url_dev = 'http://api.injoytoday.com/';
+export const base_url_live = 'https://api.injoychange.com/';
+export const current_app_build_no = '1.0.1';
+export const new_app_build_no = '1.0.2';
---
+++ source/config.js
@@ -0,0 +1,2 @@
+export const api_url = "https://test.sandebad.com/api/";
+export const token_url = "https://test.sandebad.com/";
